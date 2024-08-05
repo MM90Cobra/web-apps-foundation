5e9d8307998b19063fc4017cb15509270444b47f
Marco Michenfelder <mvsmarco@web.de>
2024-08-05 10:57:23 +0200

    implemented all features excludiung the button disabled === true reset at gameEnds()

--- 07-hangman/script.js
+++ 07-hangman/script.js
@@ -4,15 +4,33 @@ const guess = [];
 
 const outputGuess = document.querySelector("#output");
 const allButtons = document.querySelectorAll("#letters button");
+const newGameButton = document.querySelector("#gameMenu button");
+const fails = document.querySelector("#fails");
+const gameState = document.querySelector("#gameState");
+
+let countFails = 0;
 
 initGame();
 
 function initGame() {
   initGuess();
   initEventListeners();
+  fails.innerText = "Fails: 0/10";
+  countFails = 0;
+  gameState.innerText = "ACTIVE";
+
+  // allButtons.forEach(function (mybutton) {
+  //   mybutton.target.disabled = true;
+  // });
 }
 
 function initGuess() {
+  if (guess[0] !== undefined) {
+    for (const element of currentWord) {
+      guess.pop();
+    }
+  }
+
   for (const letter of currentWord) {
     guess.push("_");
   }
@@ -29,6 +47,13 @@ function renderGuess() {
 function initEventListeners() {
   allButtons.forEach(function (button) {
     button.addEventListener("click", handleLetterClick);
+    newGameButton.addEventListener("click", handleNewGameButtonClick);
+  });
+}
+
+function gameEnds() {
+  allButtons.forEach(function (button) {
+    button.removeEventListener("click", handleLetterClick);
   });
 }
 
@@ -42,11 +67,25 @@ function handleLetterClick(event) {
   // Kommt letter in currentWord vor?
   // Wenn nein: Funktion beenden
   if (currentWord.includes(letter) === false) {
+    countFails += 1;
+    console.log(countFails);
+    fails.innerText = "Fails: " + countFails + "/10";
+
+    if (countFails >= 10) {
+      console.log("Game Over");
+      gameState.innerText = "Game Over";
+      gameEnds();
+    }
+
     return;
   }
 
   // Wenn ja: Buchstabe soll an richtiger Stelle in #output ausgegeben werden
   checkGuess(letter);
+}
+
+function handleNewGameButtonClick(event) {
+  initGame();
 }
 
 function checkGuess(guessedLetter) {
@@ -61,5 +100,11 @@ function checkGuess(guessedLetter) {
     }
 
     renderGuess();
+
+    if (currentWord === outputGuess.innerText) {
+      console.log("You Win!");
+      gameState.innerText = "You Win!";
+      gameEnds();
+    }
   }
 }
