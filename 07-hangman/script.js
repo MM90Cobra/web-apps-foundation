811a864d008c0863506c98fb14fe9357545eb567
Marco Michenfelder <mvsmarco@web.de>
2024-08-05 14:53:33 +0200

    completed the task and added the extrafunction: add new word (localStorage)

--- 07-hangman/script.js
+++ 07-hangman/script.js
@@ -1,5 +1,8 @@
-const words = ["flexbox", "juni", "bootcamp", "javascript"];
-const currentWord = "flexbox";
+const defaultWords = ["flexbox", "juni", "bootcamp", "javascript"];
+
+const words = JSON.parse(localStorage.getItem("words")) || defaultWords;
+
+let currentWord = "";
 const guess = [];
 
 const outputGuess = document.querySelector("#output");
@@ -7,29 +10,35 @@ const allButtons = document.querySelectorAll("#letters button");
 const newGameButton = document.querySelector("#gameMenu button");
 const fails = document.querySelector("#fails");
 const gameState = document.querySelector("#gameState");
+const btnAdd = document.querySelector("#btn-add-word");
 
 let countFails = 0;
 
 initGame();
 
 function initGame() {
+  chooseRandomWord();
+  enableAllButtons();
   initGuess();
   initEventListeners();
   fails.innerText = "Fails: 0/10";
   countFails = 0;
   gameState.innerText = "ACTIVE";
+}
 
-  // allButtons.forEach(function (mybutton) {
-  //   mybutton.target.disabled = true;
-  // });
+function enableAllButtons() {
+  allButtons.forEach(function (mybutton) {
+    mybutton.disabled = false;
+  });
+}
+
+function chooseRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  currentWord = words[randomIndex];
 }
 
 function initGuess() {
-  if (guess[0] !== undefined) {
-    for (const element of currentWord) {
-      guess.pop();
-    }
-  }
+  guess.length = 0;
 
   for (const letter of currentWord) {
     guess.push("_");
@@ -48,6 +57,7 @@ function initEventListeners() {
   allButtons.forEach(function (button) {
     button.addEventListener("click", handleLetterClick);
     newGameButton.addEventListener("click", handleNewGameButtonClick);
+    btnAdd.addEventListener("click", addNewWord);
   });
 }
 
@@ -73,7 +83,7 @@ function handleLetterClick(event) {
 
     if (countFails >= 10) {
       console.log("Game Over");
-      gameState.innerText = "Game Over";
+      gameState.innerText = "GAME OVER";
       gameEnds();
     }
 
@@ -103,8 +113,28 @@ function checkGuess(guessedLetter) {
 
     if (currentWord === outputGuess.innerText) {
       console.log("You Win!");
-      gameState.innerText = "You Win!";
+      gameState.innerText = "YOU WIN!";
       gameEnds();
     }
   }
+}
+
+function addNewWord(event) {
+  //prevents the form element in html to refresh the site
+  event.preventDefault();
+
+  const newWordField = document.querySelector("#new-word");
+  const newWord = newWordField.value;
+  const notAcceptedCharacters = newWord.match(/[^a-z]/);
+
+  if (notAcceptedCharacters) return;
+
+  if (newWord !== "") {
+    words.push(newWord);
+    localStorage.setItem("words", JSON.stringify(words));
+
+    newWordField.value = "";
+  }
+
+  console.log(newWord);
 }
